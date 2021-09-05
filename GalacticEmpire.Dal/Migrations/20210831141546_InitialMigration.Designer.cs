f8753c347d6cf3dccb8650f10e4b44e42a7c6de5
surmannr <surmannroland@gmail.com>
2021-09-05 10:08:34 +0200

    Back to .NET 5

--- GalacticEmpire.Dal/Migrations/20210831141546_InitialMigration.Designer.cs
+++ GalacticEmpire.Dal/Migrations/20210831141546_InitialMigration.Designer.cs
@@ -4,14 +4,16 @@ using GalacticEmpire.Dal;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GalacticEmpire.Dal.Migrations
 {
     [DbContext(typeof(GalacticEmpireDbContext))]
-    partial class GalacticEmpireDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210831141546_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
