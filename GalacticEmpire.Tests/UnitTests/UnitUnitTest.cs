83a375be313f2e3031872e3cd748bb8c0ad98ffd
surmannr <surmannroland@gmail.com>
2021-10-21 23:07:29 +0200

    UnitTest-ek elkezdve

---
+++ GalacticEmpire.Tests/UnitTests/UnitUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using GalacticEmpire.Application.Features.Unit.Queries;
+using GalacticEmpire.Tests.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GalacticEmpire.Tests.UnitTests
+{
+    public class UnitUnitTest : UnitTest
+    {
+        public UnitUnitTest() : base() { }
+
+        [Fact]
+        public async Task GetAllUpgradesTest()
+        {
+            // Arrange
+            var expectedAllUnits = await _dbContext.Units.ToListAsync();
+
+            // Act
+            var query = new GetAllUnitsQuery.Query { };
+
+            var handler = new GetAllUnitsQuery.Handler(_dbContext, _mapper);
+
+            var actualAllUnits = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            actualAllUnits.Count.Should().Be(expectedAllUnits.Count);
+        }
+    }
+}
